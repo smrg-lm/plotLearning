fab1dba2a7385e7afeb6296c3cfa04ca148d1f84
Lucas Samaruga <samarugalucas@gmail.com>
2018-01-19 23:14:15 -0300

    Test updateScale

--- plotwidget.cpp
+++ plotwidget.cpp
@@ -2,7 +2,6 @@
 #include "plotview.h"
 #include "scrollview.h"
 #include "rulearea.h"
-//#include "scrollarea.h"
 
 #include <QGridLayout>
 #include <QScrollBar>
@@ -20,13 +19,19 @@ PlotWidget::PlotWidget(QWidget *parent)
     _horizontalScrollView = new ScrollView();
     _horizontalScrollView->ruleArea()->setRect(QRectF(0, 0, 500, 25));
     _layout->addWidget(_horizontalScrollView, 0, 0);
+    //connect scroll
 
     _verticalScrollView = new ScrollView();
     _verticalScrollView->setOrientation(Qt::Vertical);
     _verticalScrollView->ruleArea()->setRect(QRectF(0, 0, 25, 500));
     _layout->addWidget(_verticalScrollView, 1, 1);
+    //connect scroll
 
     _plot->setZoomOnResize(true); // TEST
-    _horizontalScrollView->setZoomOnResize(true); // TEST
-    _verticalScrollView->setZoomOnResize(true); // TEST
+    //_horizontalScrollView->setZoomOnResize(true); // TEST
+    connect(_plot, SIGNAL(scaleChanged(qreal,qreal)),
+            _horizontalScrollView, SLOT(updateScale(qreal,qreal))); // los valores son distintos
+    //_verticalScrollView->setZoomOnResize(true); // TEST
+    connect(_plot, SIGNAL(scaleChanged(qreal,qreal)),
+            _verticalScrollView, SLOT(updateScale(qreal,qreal))); // idem distinto
 }
